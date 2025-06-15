3df7e046b5e19ccfa2f63ba5462e30afa11e3b15
Abhishek Tiwari <139041595+Abshek092005@users.noreply.github.com>
2025-06-15 17:27:07 +0000

    operations

---
+++ javascript/02_operations.js
@@ -0,0 +1,16 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+ //  here it is simple that when you put negative sign before any value conatining variable it always show negative number
+ // like 3 => -3
+
+
+ let value1 = "1"
+ let value2 = 3
+ let newvalue = Number(value1)
+//  console.log(newvalue + value2);
+ 
+ 
+let Sum = 1+ 2 + "2"
+// console.log(typeof Sum);
+// console.log(Sum);
